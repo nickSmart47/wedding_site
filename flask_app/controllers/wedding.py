ae525be9dcbb78d7e2bb66d3939f291873dc3d9b
Nick Smart <nsmart147@gmail.com>
2021-12-27 22:16:38 -0500

    got google maps api working

--- flask_app/controllers/wedding.py
+++ flask_app/controllers/wedding.py
@@ -43,30 +43,5 @@ def rsvp_success():
 
 @app.route('/details')
 def details():
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-            {
-                'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-                'lat': 37.4419,
-                'lng': -122.1419,
-                'infobox': "<b>Hello World</b>"
-            },
-            {
-                'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-                'lat': 37.4300,
-                'lng': -122.1400,
-                'infobox': "<b>Hello World from other place</b>"
-            }
-        ]
-    )
+    # print(app.config['GOOGLE_MAPS_KEY'])
     return render_template('details.html', mymap=mymap, sndmap=sndmap)
