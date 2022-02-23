670a6e8fd0190b6cbdb9409d6dd78b13a6af447e
Nick Smart <nsmart147@gmail.com>
2022-02-23 16:06:43 -0500

    concealed secret keys

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,12 +3,17 @@ from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
 import logging
 
+
+import config
+
+
 app = Flask(__name__)
 
 logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
-app.secret_key = "We're getting married! Finally!!"
+app.secret_key = config.config["secret_key"]
 
-app.config['GOOGLE_MAPS_KEY'] = "AIzaSyDQoVuyT4niuC5N5aor2ydw2r8YqeQmFs8"
+app.config['GOOGLE_MAPS_KEY'] = config.config["GOOGLE_MAPS_KEY"]
 
-GoogleMaps(app, key="AIzaSyDQoVuyT4niuC5N5aor2ydw2r8YqeQmFs8")
+GoogleMaps(app, key=config.config["GOOGLE_MAPS_KEY"])
+
