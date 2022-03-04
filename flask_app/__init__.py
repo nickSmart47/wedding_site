101518bbe03c6a7b45783d95cf3f7181de015819
Nick Smart <nsmart147@gmail.com>
2022-03-04 15:23:42 -0500

    updated registry with honeymoon info,

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,19 +1,18 @@
 from flask import Flask
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
+from .settings import settings
 import logging
 
-
-import config
 
 
 app = Flask(__name__)
 
 logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
-app.secret_key = config.config["secret_key"]
+app.secret_key = settings["secret_key"]
 
-app.config['GOOGLE_MAPS_KEY'] = config.config["GOOGLE_MAPS_KEY"]
+app.config['GOOGLE_MAPS_KEY'] = settings["GOOGLE_MAPS_KEY"]
 
-GoogleMaps(app, key=config.config["GOOGLE_MAPS_KEY"])
+GoogleMaps(app, key=settings["GOOGLE_MAPS_KEY"])
 
